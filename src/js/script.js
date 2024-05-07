e7d0902b3aec45730c54422c9f851e1f7bed127d
Daniyal Samat <daniyal.samat@narxoz.kz>
2024-05-07 16:26:32 +0500

    update email send form

--- src/js/script.js
+++ src/js/script.js
@@ -16,3 +16,57 @@ const counters = document.querySelectorAll('.skills__ratings-counter'),
 counters.forEach((item, i) => {
     lines[i].style.width = item.innerHTML;
 });
+
+
+$(document).ready(function() {
+    $('#contacts-form').validate({
+        rules: {
+            name: {
+                required: true
+            },
+            email: {
+                required: true,
+                email: true
+            },
+            text: {
+                required: true
+            },
+            checkbox: {
+                required: true
+            }
+        },
+        messages: {
+            name: "Пожалуйста, введите ваше имя",
+            email: {
+                required: "Пожалуйста, введите вашу почту",
+                email: "Пожалуйста, введите корректный адрес почты"
+            },
+            text: "Пожалуйста, введите ваше сообщение",
+            checkbox: "Пожалуйста, подтвердите свое согласие с политикой конфиденциальности"
+        }
+    });
+
+    $('.modal__close').on('click', function() {
+        $('.overlay, #thanks').fadeOut('slow');
+    })
+
+
+    $('form').submit(function(e) {
+        e.preventDefault();
+
+        if(!$(this).valid()) {
+            return;
+        }
+
+        $.ajax({
+            type: "POST",
+            url: "mailer/smart.php",
+            data: $(this).serialize()
+        }).done(function() {
+            $(this).find("input").val("");
+            $('.overlay, #thanks').fadeIn('slow');
+            $('form').trigger('reset');
+        });
+        return false;
+    });
+});
